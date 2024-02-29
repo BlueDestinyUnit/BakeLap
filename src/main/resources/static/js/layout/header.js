28f293abb72373a33b1e80885d55c7e625266708
whtjdghks03 <whtjdghks03@naver.com>
2024-03-01 04:57:02 +0900

    작업완료시간 : 24.3.01 04:56

--- src/main/resources/static/js/layout/header.js
+++ src/main/resources/static/js/layout/header.js
@@ -1,16 +1,37 @@
 const searchBtn = document.querySelector('#searchBtn')
 const searchText = document.getElementById('searchText');
 
-searchBtn.onclick = get_sel_products;
+
 function get_sel_products(){
     const value = searchText.value;
     const filterParam = `search=${value}`;
     window.location.href = `/?${filterParam}`
 }
 searchText.addEventListener('keyup', function(event) {
+    console.log('안녕')
     if (event.key === 'Enter') {
         const value = searchText.value;
         const filterParam = `search=${value}`;
         window.location.href = `/?${filterParam}`
     }
+});
+
+
+document.addEventListener("DOMContentLoaded", function() {
+    // 모바일 화면 크기에서만 실행되도록 체크
+    if (window.matchMedia("(max-width: 768px)").matches) {
+        console.log('안녕')
+        searchBtn.addEventListener('click',function (e) {
+            if(searchBtn.classList.contains('ready') || searchText.value === ''){
+                e.preventDefault();
+            }else{
+                get_sel_products();
+            }
+            searchBtn.classList.toggle('ready');
+        })
+    }else{
+        searchBtn.onclick = get_sel_products;
+    }
+
+
 });
--- src/main/resources/static/js/layout/side_bar.js
+++ src/main/resources/static/js/layout/side_bar.js
@@ -4,3 +4,45 @@ categorylist.forEach(categoryLi => {
         window.location.href = `/?category=${categoryLi.className}`
     }
 } );
+
+document.addEventListener("DOMContentLoaded", function() {
+    // 모바일 화면 크기에서만 실행되도록 체크
+    if (window.matchMedia("(max-width: 768px)").matches) {
+        mobileEventHandler();
+    }
+});
+
+function mobileEventHandler() {
+    const sideBar = document.querySelector('.side-bar');
+    const sideBarIcon = document.querySelector('.side-bar-icon-box > .side-bar-icon-1');
+    const story = document.querySelector('.story');
+    const menu = document.querySelector('.menu');
+    const myPage = document.querySelector('.my-page');
+
+    story.addEventListener('click',function (e) {
+        if(sideBar.classList.contains('expanded')){
+            e.preventDefault();
+        }
+    })
+
+    menu.addEventListener('click',function (e) {
+        if(sideBar.classList.contains('expanded')){
+            e.preventDefault();
+        }
+    })
+
+    myPage.addEventListener('click',function (e) {
+        if(sideBar.classList.contains('expanded')){
+            e.preventDefault();
+        }
+    })
+
+    sideBar.addEventListener('click', function(e) {
+        sideBar.classList.toggle('expanded');
+        sideBarIcon.classList.toggle('move');
+        for (let child of sideBarIcon.children) {
+            child.classList.toggle('div-event')
+        }
+    });
+
+}
